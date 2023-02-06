2c910fa9c994dcddba5e9d3b02eaf94cb9b775ce
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-02-06 16:17:20 -0300

    starting creating the register page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import FormularioCadastro from "./Components/FormularioCadastro/FormularioCadastro";
+import {Container, Typography} from "@material-ui/core";
+import 'fontsource-roboto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Container component="article" maxWidth="sm">
+                    <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
+                    <FormularioCadastro onSubmit={SubmitFormHandler}/>
+                </Container>
+            </header>
+        </div>
+    );
 }
+    const SubmitFormHandler = (DATA) => {
+        console.log(DATA);
+    }
 
 export default App;
