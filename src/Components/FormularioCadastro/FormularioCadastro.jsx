dc976a5855d94e449822b13117c08d634561c750
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-02-07 15:23:18 -0300

    remove the switch case logic to implements array for select actual page or next

--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,34 +2,30 @@ import React from 'react';
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 import DadosEntrega from "./DadosEntrega";
-import {Typography} from "@material-ui/core";
 
 
 const FormularioCadastro = ({onSubmit}) => {
-const [etapaAtual, setEtapaAtual] = React.useState(0);
+    const [etapaAtual, setEtapaAtual] = React.useState(0);
 
-const nextEtapaHandler = () => {
-    setEtapaAtual(etapaAtual + 1);
-}
+    const nextEtapaHandler = () => {
+        setEtapaAtual(etapaAtual + 1);
+    };
 
-const FormularioAtual = (etapaAtual) => {
-    switch (etapaAtual) {
-        case 0:
-            return <DadosUsuarios aoEnviar={nextEtapaHandler}/>;
-        case 1:
-            return <DadosPessoais onSubmit={nextEtapaHandler}/>;
-        case 2:
-            return <DadosEntrega aoEnviar{onSubmit}/>;
-        default:
-            return <Typography>Erro ao selecionar etapa</Typography>;
+    const pages = [<DadosUsuarios aoEnviar={ nextEtapaHandler }/>, <DadosPessoais onSubmit={ nextEtapaHandler }/>,
+        <DadosEntrega aoEnviar={ onSubmit }/>];
+
+    let page = pages[etapaAtual];
+    for (page in pages) {
+        if (page === etapaAtual) {
+            return pages[page];
+        }
     }
-}
+
     // eslint-disable-next-line no-unreachable
-    return (
-        <>
-            {FormularioAtual(etapaAtual)}
-        </>
-    );
-}
+    return <React.Fragment>
+            { pages[etapaAtual] }
+        </React.Fragment>
+
+};
 
 export default FormularioCadastro;
